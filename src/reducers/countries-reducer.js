19541e3875fa1e416b081421c92274dffe9a104c
jaruto7 <jaruto7@gmail.com>
2018-10-15 23:52:45 +0200

    Change empty array not empty object, stupid me...

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -2,7 +2,7 @@ import { GET_COUNTRIES } from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
 const initialState = {
-    countries: {}
+    countries: []
 };
 
 const countriesReducer = function(state = initialState, action) {
